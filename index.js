90ef981f5e5e0cdc2d4a3b351454763544ee7ba5
bay <bayeldzhoroev@gmail.com>
2024-05-26 18:56:29 +0300

    Add prettier formatter to project

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import _ from 'lodash';
+import _ from "lodash";
 
-console.log('Hello, Helxet!');
-console.log(_.last(['one', 'two']));
+console.log("Hello, Helxet!");
+console.log(_.last(["one", "two"]));
